8f0a32a9298cae33b89d4a8779e085402f1e4cd5
Vitaly Kiselev <st411ar@ya.ru>
2019-07-23 14:32:00 +0300

    implemented common setup

--- AccountTest.cs
+++ AccountTest.cs
@@ -5,15 +5,22 @@ namespace Bank
     [TestFixture]
     public class AccountTest
     {
+        Account source;
+        Account destination;
+
+        [SetUp]
+        public void Init()
+        {
+            source = new Account();
+            source.Deposit(200m);
+
+            destination = new Account();
+            destination.Deposit(150m);
+        }
+
         [Test]
         public void TransferFunds()
         {
-            Account source = new Account();
-            source.Deposit(200m);
-
-            Account destination = new Account();
-            destination.Deposit(150m);
-
             source.TransferFunds(destination, 100m);
 
             Assert.AreEqual(250m, destination.Balance);
@@ -24,12 +31,6 @@ namespace Bank
         [ExpectedException(typeof(InsufficientFundsException))]
         public void TransferWithInsufficientFunds()
         {
-            Account source = new Account();
-            source.Deposit(200m);
-
-            Account destination = new Account();
-            destination.Deposit(150m);
-
             source.TransferFunds(destination, 300m);
         }
 
@@ -37,12 +38,6 @@ namespace Bank
         [Ignore("Decide how to implement transaction management")]
         public void TransferWithInsufficientFundsAtomicity()
         {
-            Account source = new Account();
-            source.Deposit(200m);
-
-            Account destination = new Account();
-            destination.Deposit(150m);
-
             try
             {
                 source.TransferFunds(destination, 300m);
